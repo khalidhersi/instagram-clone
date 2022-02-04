bf49a2b6962fa3657466521ee73275994a438671
khalidhersi <cipherpolfoundation@gmail.com>
2022-02-04 11:10:59 +0000

    nav bar styling complete

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -9,15 +9,18 @@ import heart from "../../assets/images/heart.png";
 import user from "../../assets/images/user.png";
 
 const Nav = () => {
-  return <div className='NavBar'>
-    <ul className='NavBar__list'>
-    <h1 className='NavBar__logo '>𝓘𝓷𝓼𝓽𝓪𝓰𝓻𝓪𝓶</h1>
-      <img src={home} alt="home logo" className='NavBar__list__items'/>
-      <img src={directMessage} alt="directMessage logo" className='NavBar__list__items'/>
-      <img src={more} alt="more logo" className='NavBar__list__items'/>
-      <img src={broadcast} alt="broadcast logo" className='NavBar__list__items'/>
-      <img src={heart} alt="heart logo" className='NavBar__list__items'/>
-      <img src={user} alt="user logo" className='NavBar__list__items'/>
+  return <div className='navbar'>
+    {/* <h1 className='NavBar__logo '>𝓘𝓷𝓼𝓽𝓪𝓰𝓻𝓪𝓶</h1> */}
+    <div className="navbar__logo">
+    <img src={instaLogo} alt="logo" className='navbar__list__items navbar__logo__items'/>
+    </div>
+    <ul className='navbar__list'>
+      <img src={home} alt="home logo" className='navbar__list__items'/>
+      <img src={directMessage} alt="directMessage logo" className='navbar__list__items'/>
+      <img src={more} alt="more logo" className='navbar__list__items'/>
+      <img src={broadcast} alt="broadcast logo" className='navbar__list__items'/>
+      <img src={heart} alt="heart logo" className='navbar__list__items'/>
+      <img src={user} alt="user logo" className='navbar__list__items'/>
     </ul>
   </div>;
 };
